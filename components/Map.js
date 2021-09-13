8499f2fcd7ed114016727f6842903f91dfe71dfc
Dharmesh Kumar Radhakrishnan <dharmeshkumarradhakrishnan@Dharmeshs-MacBook-Pro.local>
2021-09-14 00:42:55 +0530

    day-4, map integration

---
+++ components/Map.js
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import getCenter from "geolib/es/getCenter";
+
+function Map({ searchResults }) {
+  const [selectedLocation, setSelectedLocation] = useState({});
+
+  const coordinates = searchResults.map((result) => {
+    return {
+      latitude: result.lat,
+      longitude: result.long,
+    };
+  });
+
+  const center = getCenter(coordinates);
+
+  const [viewport, setViewport] = useState({
+    latitude: center.latitude,
+    longitude: center.longitude,
+    width: "100%",
+    height: "100%",
+    zoom: 10,
+  });
+
+  return (
+    <ReactMapGL
+      mapStyle="mapbox://styles/dharmeshinfinijith/cktiyvfbe5tvk18pm6olmohjl"
+      mapboxApiAccessToken={process.env.mapbox_key}
+      {...viewport}
+      onViewportChange={(viewport) => setViewport(viewport)}
+    >
+      {searchResults.map((result) => (
+        <div key={result.long}>
+          <Marker
+            longitude={result.long}
+            latitude={result.lat}
+            offsetTop={-10}
+            offsetLeft={-20}
+          >
+            <p
+              role="img"
+              aria-label="push-pin"
+              onClick={() => setSelectedLocation(result)}
+              className="cursor-pointer text-2xl animate-bounce"
+            >
+              📍
+            </p>
+          </Marker>
+
+          {selectedLocation.long === result.long ? (
+            <Popup
+              closeOnClick={true}
+              onClose={() => setSelectedLocation({})}
+              latitude={result.lat}
+              longitude={result.long}
+            >
+              {result.title}
+            </Popup>
+          ) : (
+            false
+          )}
+        </div>
+      ))}
+    </ReactMapGL>
+  );
+}
+
+export default Map;
--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-    images: {
-        domains: ['links.papareact.com'],
-    },
+  images: {
+    domains: ["links.papareact.com"],
+  },
+  env: {
+    mapbox_key:
+      "pk.eyJ1IjoiZGhhcm1lc2hpbmZpbmlqaXRoIiwiYSI6ImNrdGl5cjJhczE2bnEyd2pvZHpqangzZ2sifQ.oxXeZSAYCa9voNbG1Kkt5A",
+  },
 };
